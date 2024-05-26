c358fbd8233c7abbaebab18a2c61f3b6fd4183fd
ljtc <ljtc@ciencias.unam.mx>
2024-05-25 18:51:41 -0600

    La única unifdad es el nuetro

--- MyProject/Graphic.lean
+++ MyProject/Graphic.lean
@@ -25,10 +25,25 @@ example {M : Type} [GraphicMonoid M] (a : M) : a * 1 = a := by
 example {M : Type} [GraphicMonoid M] (a : M) : 1 * a = a := by
   rw [one_mul]
 
+
+/-
+Multiplication of Graphic Monoids is idempotent
+-/
 theorem elem_idem {M : Type} [GraphicMonoid M] (a : M) : a * a = a := by
   calc a * a
     _ = a * 1 * a := by rw [mul_one]
     _ = a * 1     := by rw [GraphicMonoid.graphic_id a 1]
     _ = a         := by rw [mul_one]
+
+/-
+The only unit of a Graphic Monoid is the identity
+-/
+theorem unit_one {M : Type} [GraphicMonoid M] (a b : M) (h : a * b = 1) :
+    a = 1 := by
+  calc
+    a = 1 * a     := by rw [one_mul]
+    _ = a * b * a := by rw [h]
+    _ = a * b     := by rw [GraphicMonoid.graphic_id]
+    _ = 1         := by rw [h]
 
 end Graphic
