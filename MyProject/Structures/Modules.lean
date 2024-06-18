fed4de5166593a8a2e93772b5f27dd62f40e7fb0
ljtc <ljtc@ciencias.unam.mx>
2024-06-18 17:15:53 -0600

    modules

--- MyProject/Structures/Modules.lean
+++ MyProject/Structures/Modules.lean
@@ -21,6 +21,7 @@ variable (R V : Type*) [Field R] [AddCommGroup V] [Module R V]
 #check add_smul
 #check one_smul
 #check smul_smul
+--posiblemente más pero ya lo olvidé
 
 
 namespace MyR2
@@ -46,17 +47,49 @@ def zero : R2 := ⟨0, 0⟩
 
 def smul (r : ℝ) (a : R2) : R2 := ⟨r * a.x, r * a.y⟩
 
-protected theorem add_assoc (a b c : R2) : add (add a b) c = add a (add b c) := sorry
+protected theorem add_assoc (a b c : R2) :
+    add (add a b) c = add a (add b c) := sorry
+
+protected theorem add_comm (a b : R2) : add a b = add b a := sorry
+
+protected theorem zero_add (a : R2) : add zero a = a := sorry
+
+protected theorem add_zero (a : R2) : add a zero = a := sorry
+
+def nsmul := fun (n : ℕ) ↦ fun (a : R2) ↦ smul ↑n a
+
+protected theorem nsmul_zero : ∀ a, nsmul 0 a = zero := sorry
+
+protected theorem nsmul_succ :
+    ∀ (n : ℕ) (a), nsmul (n + 1) a = add (nsmul n a) a := sorry
 
 
 instance : AddCommMonoid R2 where
-  add := add
+  add := sorry
   add_assoc := sorry
-  zero := zero
+  zero := sorry
   zero_add := sorry
   add_zero := sorry
-  nsmul := sorry
   add_comm := sorry
+  nsmul := sorry
+  nsmul_zero := sorry
+  nsmul_succ := sorry
+
+
+protected theorem one_smul (a : R2) : smul (1 : ℝ) a = a := sorry
+
+protected theorem mul_smul (r s : ℝ) (a : R2) :
+    smul (r * s) a = smul r (smul s  a) := sorry
+
+protected theorem smul_zero (r : ℝ) : smul r zero = zero := sorry
+protected theorem smul_add (r : ℝ) (a b : R2) :
+    smul r (add a b) = add (smul r a) (smul r b) := sorry
+
+protected theorem add_smul (r s : ℝ) (a : R2) :
+    smul (r + s) a = add (smul r a) (smul s a) := sorry
+
+protected theorem zero_smul (a : R2) : smul (0 : ℝ) a = zero := sorry
+
 
 
 instance : Module ℝ R2 where
@@ -70,3 +103,7 @@ instance : Module ℝ R2 where
 
 
 end MyR2
+
+
+--Theorem 1.1 (Cancelation Law for Vector Adition)
+example (x y z : V) (h : x + z = y + z) : x = y := sorry
